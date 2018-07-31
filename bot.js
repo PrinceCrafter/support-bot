d2fefbc5ca1559ddea04a8824be31adb3873c99e
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-31 12:16:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = ",";
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+const prefix = "^";
 
-client.on('message', message => {
-    if (message.content === 'zg') {
-        message.reply('pong');
-      }
+client.on('ready', function(){    
+    var ms = 40000 ;    
+    var setGame = ['Jungle | 1.4k Member'];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
+}, ms);    
+    
 });
-
 
 
 client.login(process.env.BOT_TOKEN);  //اياكككك تلعب هنا لا تحط توكنك هنا 
